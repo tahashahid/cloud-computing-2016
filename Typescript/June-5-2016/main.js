4f878c2aaa75b8445a33ccf46d4888f2ba270173
tahashahid <m.tahashahid@gmail.com>
2016-06-06 13:44:59 +0500

    Class Code June-5-2016

---
+++ Typescript/June-5-2016/main.js
@@ -0,0 +1,16 @@
+// let myType = {name: "taha", age: 10};
+// // myType = {name: "zia", age: 10, gender: true};
+// let myType2 = {name: "shahid", age: 100, id: 10};
+// myType = myType2;
+// console.log(myType.name);
+// 
+// let arr : Array<number> = new number[2];
+// let a : number = 5;
+// let b : any = 9;
+if (true) {
+    var a = 10;
+    if (true) {
+        var a_1 = 20;
+        console.log(a_1);
+    }
+}
---
+++ Typescript/June-5-2016/main.ts
@@ -0,0 +1,28 @@
+// let myType = {name: "taha", age: 10};
+// // myType = {name: "zia", age: 10, gender: true};
+// let myType2 = {name: "shahid", age: 100, id: 10};
+// myType = myType2;
+// console.log(myType.name); //warning
+// 
+
+// let myType: {foo: number, bar?: number};
+
+//let array : number[] = [];
+// let arr : Array<number> = new number[2]; //error 
+
+// let a : number = 5;
+// let b : any = 9;
+
+
+// if(true){
+//     let a = 10;
+//     if(true){
+//         let a = 20
+//         console.log(a)
+//     }
+// }
+
+// let myType : any= <any> { name: "Zia", id: 1 };
+
+
+
