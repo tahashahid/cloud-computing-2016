6237d38f2083e25d02dbae9f8b1fb520fc729d02
adeelhussain <mr.adi180@gmail.com>
2016-10-18 12:57:01 +0500

    Oct-16-2016 Class Code

---
+++ Angular2/Oct-16-2016/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloWorldComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
